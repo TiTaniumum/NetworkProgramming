3dc440fcbabfe55f98b525cd1dd139313138f3db
yelemanov.temirlan@gmail.com <yelemanov.temirlan@gmail.com>
2023-09-21 17:55:53 +0600

    code upgraded

--- Net1_2/Form1.cs
+++ Net1_2/Form1.cs
@@ -20,8 +20,18 @@ namespace Net1_2
         {
             InitializeComponent();
             InitList();
+            InitPoolThread();
+            MessageHandle = SaveToLog;
         }
         private Socket serverSocket = null;
+        public delegate void dSaveToLog(string msg);
+        public dSaveToLog MessageHandle;
+        private List<string> Clients;
+        private void InitPoolThread()
+        {
+            ThreadPool.SetMinThreads(10,10);
+            ThreadPool.SetMaxThreads(int.MaxValue, int.MaxValue);
+        }
         private void InitList()
         {
             string compName = Dns.GetHostName();
@@ -54,18 +64,126 @@ namespace Net1_2
             int port;
             if (!int.TryParse(textBoxPort.Text, out port) || port <=0 || port>65535)
             {
-                MessageBox.Show("Port is not Correct");
+                textBoxLog.Text+="ERROR: Port is not Correct!\r\n";
+                //MessageBox.Show("Port is not Correct");
                 textBoxPort.Focus();
                 textBoxPort.SelectAll();
+                return;
+            }
+            IPAddress ip;
+            if (!IPAddress.TryParse(comboBoxServerIP.Text,out ip))
+            {
+                textBoxLog.Text+="ERROR: IP is not correct";
+                comboBoxServerIP.Focus();
+                comboBoxServerIP.SelectAll();
+                return;
             }
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //serverSocket.Bind();
-            //serverSocket.Listen();
+            serverSocket.Bind(new IPEndPoint(ip, port));
+            serverSocket.Listen(100);
+
+            //ThreadPool.QueueUserWorkItem();
+
+            buttonStart.Text = "Stop";
         }
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void ServerProc(object obj)
+        {
+            Form1 form = obj as Form1;
+            form.Invoke(MessageHandle, "Server started");
+            while (true)
+            {
+                try
+                {
+                    form.Invoke(MessageHandle, "Waiting next client...");
+                    Socket client = form.serverSocket.Accept();
+                    //client param
+
+                    ClientParam param = new ClientParam {
+                        client = client,
+                        form = form,
+                    };
+                    form.Invoke(MessageHandle, $"Client {client.RemoteEndPoint} is connected");
+                    //ThreadPool.QueueUserWorkItem(ClientProc, param);
+                }
+                catch(Exception ex)
+                {
+                    form.Invoke(MessageHandle, ex.Message);
+                    break;
+                }//try
+            }//while
+            
+        }
+        private void ClientProc(object obj)
+        {
+            ClientParam par = (ClientParam)obj;
+            try
+            {
+                //1 hello
+                par.form.Invoke(MessageHandle, "Client Thread is runing, sending \'Hello!\'");
+                string msg = "Hello!";
+                par.client.Send(Encoding.UTF8.GetBytes(msg));
+                //2 getname
+                byte[] buffer = new byte[4*1024];
+                int size = par.client.Receive(buffer);
+                par.name = Encoding.UTF8.GetString(buffer,0,size);
+                par.form.Invoke(MessageHandle, $"Client: {par.name}");
+                //3 comands
+                while (true)
+                {
+                    size = par.client.Receive(buffer);
+                    if (size<=0)
+                    {
+                        break;
+                    }
+                    string command = Encoding.UTF8.GetString(buffer,0,size);
+                    par.form.Invoke(MessageHandle, $"\'{par.name}\' sent command: \'{command}\'");
+                    switch (command)
+                    {
+                        case "GetListClient": 
+                            break;
+                        case "SendMessageAll": 
+                            break;
+                        case "SendMessageTo": 
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+
+            }catch(Exception ex)
+            {
+                par.form.Invoke(MessageHandle, ex.Message);
+            }
+            finally
+            {
+                par.form.Invoke(MessageHandle, $"User {par.name} disconnected!" );
+                par.client.Close();
+                //delete client from list
+                //par.form.clientList.remove(par);
+            }
+        }
+        public void SaveToLog(string msg)
+        {
+            textBoxLog.Text += msg+ "\r\n";
+        }
+        public struct ClientParam
+        {
+            public string name;
+            public Socket client;
+            public Form1 form;
+            public ClientParam(string name, Socket client, Form1 form)
+            {
+                this.name = name;
+                this.client = client;
+                this.form = form;
+            }
         }
     }
 }
