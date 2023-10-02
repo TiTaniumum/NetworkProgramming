fd7ce3966b0f0d11d30365f44428da1cbdc6e994
yelemanov.temirlan@gmail.com <yelemanov.temirlan@gmail.com>
2023-10-02 18:01:16 +0600

    UDP

---
+++ UdpClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace NetUdpClient
+{
+    internal class MainClass
+    {
+        static IPAddress server = IPAddress.Parse("127.0.0.1");
+        static int port = 12345;
+        static Socket socket;
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.ReadLine();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string msg = "Hello! This is client!";
+            int size = socket.SendTo(Encoding.UTF8.GetBytes(msg), new IPEndPoint(server, port));
+            Console.WriteLine("Send message to UDP-Server");
+            Console.WriteLine($"Send size {size} bytes");
+            socket.Close();
+            string fileName = "";
+            int blockSize = 1024;
+            FileInfo file = new FileInfo(fileName);
+            int cntSend = (int)(file.Length/blockSize);
+            int remSend = (int)(file.Length%blockSize);
+            UdpClient uClient = new UdpClient();
+            EndPoint srvEP = new IPEndPoint(server, port);
+            char[] buf = new char[blockSize];
+            int startIndex = 0;
+            Convert.ToByte();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                sr.ReadBlock(buf,startIndex, blockSize);
+                uClient.Send();
+            }
+        }
+    }
+}
---
+++ UdpServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace UdpServer
+{
+    internal class MainClass
+    {
+        static int port = 12345;
+        static Socket socket;
+        static byte[] buffer = new byte[64*1024];
+        public static void Main(string[] args)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            EndPoint localEP = new IPEndPoint(IPAddress.Any, port);
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, port);
+            socket.Bind(localEP);
+            Console.WriteLine($"Start UDP-Server on: {localEP}");
+            int size = socket.ReceiveFrom(buffer, ref remoteEP);
+            Console.WriteLine($"Receive data from: {remoteEP}");
+            Console.WriteLine($"Receive data size: {size}");
+            string msg = Encoding.UTF8.GetString(buffer, 0, size);
+            Console.WriteLine($"Receive data size: \n{msg}");
+            socket.Close();
+        }
+    }
+}
