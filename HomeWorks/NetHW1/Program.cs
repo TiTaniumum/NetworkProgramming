035eb929e414d4168b634b0da2237d3818e3933d
TiTaniumum <36527788+TiTaniumum@users.noreply.github.com>
2023-10-06 00:35:13 +0600

    Home Work 1

---
+++ HomeWorks/NetHW1/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Net.Security;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+namespace NetHW1
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            PrepareThreads();
+            Server server = new Server();
+            server.StartServer();
+            server.ManageServer();
+        }
+        public static void PrepareThreads()
+        {
+            ThreadPool.SetMaxThreads(100, 100);
+            ThreadPool.SetMinThreads(100, 100);
+        }
+    }
+    public class Server
+    {
+        static Socket server = null;
+        static bool isOn = false;
+        Thread serverThread = null;
+        public Server()
+        {
+            if (server != null) return;
+            Init();
+        }
+        private void Init(int port = 63997)
+        {
+            IPEndPoint ipEP = new IPEndPoint(IPAddress.Any, port);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipEP);
+        }
+        public void StartServer()
+        {
+            Console.WriteLine("Starting Server....");
+            if (server == null) Init();
+            if (isOn)
+            {
+                Console.WriteLine("Server is already running!");
+                return;
+            }
+            isOn = true;
+            serverThread = new Thread(ServerProc);
+            serverThread.IsBackground = true;
+            serverThread.Start(server);
+        }
+        public void StopServer()
+        {
+            Console.WriteLine("Shutting down server....");
+            isOn = false;
+            //Я хотел прервать Accept() что бы выключить сервер. но этот метод уже не поддерживается.
+            //serverThread.Abort();
+            server.Close();
+            server = null;
+            serverThread.Join();
+            Console.WriteLine("Server has Stopped!");
+        }
+        public void ManageServer()
+        {
+            Console.WriteLine("To Stop Server Write \'STOP\'; To get Info about Server \'INFO\'");
+            while (isOn)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "stop":
+                    case "STOP":
+                        StopServer();
+                        break;
+                    case "INFO":
+                    case "info":
+                        Console.WriteLine("SERVER INFO:");
+                        Console.WriteLine("Connected: " + server.Connected.ToString());
+                        Console.WriteLine("Address family: "+server.AddressFamily.ToString());
+                        Console.WriteLine("Remote end point: "+server.RemoteEndPoint?.ToString());
+                        Console.WriteLine("Socket type: "+server.SocketType.ToString());
+                        Console.WriteLine("Protocol type: "+server.ProtocolType.ToString());
+                        break;
+                    case "?":
+                    case "help":
+                    case "HELP":
+                    default:
+                        Console.WriteLine("To Stop Server Write \'STOP\'; To get Info about Server 'INFO'");
+                        break;
+                }
+            }
+        }
+
+        private void ServerProc(object serverObj)
+        {
+            Socket server = serverObj as Socket;
+            server.Listen(1000);
+            Console.WriteLine("Server: " + server.LocalEndPoint.ToString());
+            Console.WriteLine("Starting to Listen...");
+            try
+            {
+                while (isOn)
+                {
+                    Socket client = server.Accept();
+                    Console.WriteLine("Client Accepted");
+                    ServerSideClient serverSideClient = new ServerSideClient(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                isOn = false;
+            }
+            Console.WriteLine("Server is Shutting Down");
+        }
+    }
+    public class ServerSideClient
+    {
+        Socket client;
+        public ServerSideClient(Socket client)
+        {
+            this.client = client;
+            ThreadPool.QueueUserWorkItem(ClientProc, client);
+        }
+        private void ClientProc(object clientObj)
+        {
+            Socket client = clientObj as Socket;
+            string EP = client.RemoteEndPoint.ToString();
+            byte[] buffer = new byte[1024];
+            int size;
+            string message = "[1] - Date, [2] - Time, [3] - Disconnect, [4] - Info";
+            Console.WriteLine("Client connected: " + client.LocalEndPoint.ToString() + " => " + client.RemoteEndPoint.ToString());
+            client.Send(Encoding.UTF8.GetBytes(message));
+            while (client.Connected)
+            {
+                size = client.Receive(buffer);
+                message = Encoding.UTF8.GetString(buffer, 0, size);
+                Console.WriteLine("Received message " + message + " from " + client.RemoteEndPoint.ToString());
+                switch (message)
+                {
+                    case "1":
+                        message = DateTime.Now.ToString("dd-MM-yyyy");
+                        Send(message);
+                        break;
+                    case "2":
+                        message = DateTime.Now.ToString("hh:mm:ss:ffff");
+                        Send(message);
+                        break;
+                    case "3":
+                        client.Close();
+                        //Send("Disconnecting!");
+                        break;
+                    case "4":
+                        Send("[1] - Date, [2] - Time, [3] - Disconnect, [4] - Info");
+                        break;
+                    default:
+                        Send("Wrong input!");
+                        break;
+                }
+            }
+            Console.WriteLine(EP + " Has disconnected!");
+            //client.Close();
+        }
+        private byte[] Prepare(string message) => Encoding.UTF8.GetBytes(message);
+        private void Send(string message) => client.Send(Prepare(message));
+    }
+}
---
+++ HomeWorks/NetHW1_1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+
+namespace NetHW1_1
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+        }
+    }
+    public class Client
+    {
+        static Socket client = null;
+        IPEndPoint ipEP = null;
+        bool isConnected = false;
+        public Client()
+        {
+            if (client == null) return;
+            Init();
+        }
+        private void Init(int port = 63997)
+        {
+            if (ipEP == null)
+                ipEP = new IPEndPoint(IPAddress.Loopback, 63997);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        private void Connect()
+        {
+            if (client == null) Init();
+            if (client.Connected)
+            {
+                Console.WriteLine("You are already connected!");
+                return;
+            }
+            Console.WriteLine("Connecting to server...");
+            try
+            {
+                for (int i = 0; i < 3 && !client.Connected; i++, Thread.Sleep(1000))
+                    client.Connect(ipEP);
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to connect: {ex.Message}");
+                return;
+            }
+
+            if (!client.Connected)
+            {
+                Console.WriteLine("Connection failed...");
+                return;
+            }
+            isConnected = true;
+            Thread listenerThread = new Thread(ListenProc);
+            listenerThread.IsBackground = true;
+            listenerThread.Start();
+        }
+        public void Start()
+        {
+            Connect();
+            string message = string.Empty;
+            if (client.Connected) Console.WriteLine("Connection successfull!");
+            Console.WriteLine("COMMANDS: \'STOP\', \'CONNECT\', \'HELP\';");
+            Console.WriteLine("ANY OTHER INPUT WILL BE SENT TO SERVER.");
+            while (message != "STOP")
+            {
+                message = ""+Console.ReadLine();
+                switch (message)
+                {
+                    case "CONNECT":
+                        Connect();
+                        break;
+                    case "stop":
+                    case "STOP":
+                    case "3":
+                        if (isConnected)
+                        {
+                            Send("3");
+                            isConnected = false;
+                            client.Disconnect(false);
+                            ShutDown();
+                        }
+                        break;
+                    case "?":
+                    case "help":
+                    case "HELP":
+                        Console.WriteLine("COMMANDS: \'STOP\', \'CONNECT\', \'HELP\';");
+                        Console.WriteLine("ANY OTHER INPUT WILL BE SENT TO SERVER.");
+                        break;
+                    default:
+                        if (client.Connected) Send(message);
+                        break;
+                }
+            }
+            Console.WriteLine("Shutting down client...");
+            ShutDown();
+        }
+        private void ListenProc(object obj)
+        {
+            byte[] buffer = new byte[1024];
+            int size;
+            try
+            {
+                while (isConnected)
+                {
+                    size = client.Receive(buffer);
+                    Console.WriteLine("SERVER: " + Encoding.UTF8.GetString(buffer, 0, size));
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }finally {
+                ShutDown();
+            }
+            Console.WriteLine("Disconnected.");
+        }
+        private void ShutDown()
+        {
+            Thread.Sleep(100);
+            isConnected = false;
+            client?.Close();
+            client = null;
+        }
+        private byte[] Prepare(string message) => Encoding.UTF8.GetBytes(message);
+        private void Send(string message) => client.Send(Prepare(message));
+    }
+}
