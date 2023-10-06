4dadb442283aa5ce271d74a08168d46972574b39
TiTaniumum <36527788+TiTaniumum@users.noreply.github.com>
2023-10-06 23:03:39 +0600

    Home Work 4

---
+++ HomeWorks/NetHW4/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace NetHW4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBoxServerIP = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listViewClients = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(409, 31);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(93, 25);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // comboBoxServerIP
+            // 
+            this.comboBoxServerIP.FormattingEnabled = true;
+            this.comboBoxServerIP.Location = new System.Drawing.Point(12, 31);
+            this.comboBoxServerIP.Name = "comboBoxServerIP";
+            this.comboBoxServerIP.Size = new System.Drawing.Size(184, 24);
+            this.comboBoxServerIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server IP";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(226, 31);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(88, 22);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // listViewClients
+            // 
+            this.listViewClients.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewClients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewClients.HideSelection = false;
+            this.listViewClients.Location = new System.Drawing.Point(12, 77);
+            this.listViewClients.Name = "listViewClients";
+            this.listViewClients.Size = new System.Drawing.Size(490, 143);
+            this.listViewClients.TabIndex = 5;
+            this.listViewClients.UseCompatibleStateImageBehavior = false;
+            this.listViewClients.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "№";
+            this.columnHeader1.Width = 30;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "RemoteAddress";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "MessageCount";
+            this.columnHeader4.Width = 100;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Clients";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Log";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLog.Location = new System.Drawing.Point(12, 242);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(490, 196);
+            this.textBoxLog.TabIndex = 8;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonClose.Location = new System.Drawing.Point(190, 457);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(105, 46);
+            this.buttonClose.TabIndex = 9;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 515);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listViewClients);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxServerIP);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox comboBoxServerIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listViewClients;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ HomeWorks/NetHW4/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetHW4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            server = Server.getInstance(this);
+        }
+        Server server;
+        public void buttonStart_Click(object sender,EventArgs e)
+        {
+            // TODO: start server.
+        }
+        public void buttonClose_Click(object sender,EventArgs e)
+        {
+            // TODO: stop Server, close Form.
+        }
+        public void Log(string message)
+        {
+            textBoxLog.Invoke((Action)delegate
+            {
+                textBoxLog.Text += message+"\r\n";
+            });
+        }
+    }
+
+    public class Server
+    {
+        private static Socket server = null;
+        private static Server instance = null;
+        private List<ServerSideClient> clients = null;
+        private bool isOn = false;
+        Form1 Form { get; set; }
+        private Server(Form1 form)
+        {
+            this.Form=form;
+            if (server == null) server = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            if (clients == null) clients = new List<ServerSideClient>();
+        }
+        public static Server getInstance(Form1 form)
+        {
+            if (instance == null) instance = new Server(form);
+            else instance.Form=form;
+            return instance;
+        }
+        public static Server getInstance()
+        {
+            return instance;
+        }
+        public void Start(IPAddress ip, int port)
+        {
+            IPEndPoint ipEP = new IPEndPoint(ip, port);
+            server.Bind(ipEP);
+            server.Listen(1000);
+            Thread serverThread = new Thread(ServerProc);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+        private void ServerProc()
+        {
+            isOn = true;
+            while (isOn)
+            {
+                Socket client = server.Accept();
+            }
+        }
+        public void Stop()
+        {
+
+        }
+        public void Log(string message)
+        {
+            Form.Log(message);
+        }
+    }
+    public class ServerSideClient
+    {
+        private Socket client;
+        private Server server = Server.getInstance();
+        public ServerSideClient(Socket client)
+        {
+            this.client = client;
+            ThreadPool.QueueUserWorkItem(ClientProc);
+        }
+        private void ClientProc(object obj)
+        {
+            byte[] buffer = new byte[10*1024];
+            string message = string.Empty;
+            
+            while (client.Connected)
+            {
+                message = GetMessage(buffer);
+                server.Log(message);
+                HandleRequest(message);
+            }
+        }
+        private string GetMessage(byte[] buffer)
+        {
+            int size = client.Receive(buffer);
+            return Encoding.UTF8.GetString(buffer, 0, size);
+        }
+        private void HandleRequest(string message)
+        {
+            string command = message.Split(':')[0];
+            string actualMessage = message.Split(':')[1];
+            switch (command)
+            {
+                case "ALL":
+                    //SendToEveryone(actualMessage);
+                    break;
+            }
+        }
+        private byte[] Prepare(string message) => Encoding.UTF8.GetBytes(message);
+        private void Send(string message) => client.Send(Prepare(message));
+    }
+}
