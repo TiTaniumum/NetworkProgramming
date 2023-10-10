0b192a7b62f965b2e4e3751b95de751f9f498dad
yelemanov.temirlan@gmail.com <yelemanov.temirlan@gmail.com>
2023-10-10 18:01:35 +0600

    broadcast

---
+++ Net4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Net4
+{
+    internal class MianClass
+    {
+        static void Main(string[] args)
+        {
+            //FtpWebRequest ftpWebReq = (FtpWebRequest)FtpWebRequest.Create("ftp://ftp.gnu.org");
+            //IPEndPoint ipEP = new IPEndPoint(IPAddress.Any, 12345);
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint remoteEP = new IPEndPoint(0,0);
+            byte[] buffer;
+            while (true)
+            {
+                buffer = udpClient.Receive(ref remoteEP);
+                string str = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"{remoteEP}: {str}");
+                if (str == "Exit") break;
+            }
+            Console.WriteLine("EXITed");
+            udpClient.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Net4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Net4_1
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint multiCastEP = new IPEndPoint(IPAddress.Parse("10.13.12.255"), 12345);
+            // 224.255.255.255
+            string message;
+            byte[] buffer;
+            while (true)
+            {
+                message = Console.ReadLine();
+                if (message == null || message.Length == 0) continue;
+                buffer = Encoding.UTF8.GetBytes(message);
+                udpClient.Send(buffer, multiCastEP);
+                if (message == "Exit") break;
+            }
+            udpClient.Close();
+            Console.ReadLine();
+        }
+    }
+}
